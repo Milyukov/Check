cf5eb204fecc9f19ae03ea6700eadf4677d9da8c
glebm <cvresearch.milyukov@gmail.com>
2017-03-19 14:21:46 +0300

    + can transmitt data from java part to cpp and back

--- TotalCheque/app/src/main/java/com/lab_in_the_pocket/totalcheque/MainActivity.java
+++ TotalCheque/app/src/main/java/com/lab_in_the_pocket/totalcheque/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lab_in_the_pocket.totalcheque;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -18,12 +19,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        getPriceTagInfo(bitmap);
+        tv.setText("Bitmap width now is " + bitmap.getWidth());
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native void getPriceTagInfo(Bitmap bitmap);
 }
