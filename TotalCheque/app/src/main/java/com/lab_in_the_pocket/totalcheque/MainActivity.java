b656d3e38f8ec06cb45a69ef61a0eac6b0a3c630
gleb.milyukov <cvresearch.milyukov@gmail.com>
2017-04-11 22:48:11 +0300

    bug fix

--- TotalCheque/app/src/main/java/com/lab_in_the_pocket/totalcheque/MainActivity.java
+++ TotalCheque/app/src/main/java/com/lab_in_the_pocket/totalcheque/MainActivity.java
@@ -3,24 +3,31 @@ package com.lab_in_the_pocket.totalcheque;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("label-parser-lib");
+        System.loadLibrary("price-tag-parser-lib");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("onCreate", "");
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-        getPriceTagInfo(bitmap);
+        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.RGB_565);
+        try {
+            getPriceTagInfo(bitmap);
+        }
+        catch (Exception e) {
+            Log.e("", e.getMessage());
+        }
         tv.setText("Bitmap width now is " + bitmap.getWidth());
     }
 
